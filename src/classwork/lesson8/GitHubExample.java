4471a546e9b05ebc343b04ca48a931c24f7bb6a4
Narekkkk <narekkhachatryan197@gmail.com>
2023-04-17 20:16:08 +0400

    First project

--- src/classwork/lesson8/GitHubExample.java
+++
@@ -1,7 +0,0 @@
-package classwork.lesson8;
-
-public class GitHubExample {
-    public static void main(String[] args) {
-        System.out.print("This is Github example");
-    }
-}
---
+++ src/classwork/lesson8/GithubExample.java
@@ -0,0 +1,7 @@
+package classwork.lesson8;
+
+public class GithubExample {
+    public static void main(String[] args) {
+        System.out.println("This is Github example");
+    }
+}
