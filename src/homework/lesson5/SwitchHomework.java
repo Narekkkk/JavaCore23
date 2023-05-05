87fe669ae83ef5d2c1b0e1f1b944f739bc3e772a
Narekkkk <narekkhachatryan197@gmail.com>
2023-05-05 22:13:22 +0400

    First project

---
+++ src/homework/lesson5/SwitchHomework.java
@@ -0,0 +1,39 @@
+package homework.lesson5;
+
+public class SwitchHomework {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println(i);
+                    break;
+                case 1:
+                    System.out.println(i);
+                    break;
+                case 2:
+                    System.out.println(i);
+                    break;
+                case 3:
+                    System.out.println(i);
+                    break;
+                case 4:
+                    System.out.println(i);
+                    break;
+                case 5:
+                    System.out.println(i);
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                default:
+                    System.out.println("i < 5");
+            }
+
+        }
+    }
+}
